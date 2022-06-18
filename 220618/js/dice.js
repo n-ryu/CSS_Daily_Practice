76d841194ccef9175541f792fe676d8ae5119897
n-ryu <ababc100564@gmail.com>
2022-06-18 21:13:04 +0900

    220618 minor update
    - colors and 3d-like effect added (using inset shadow)

--- 220618/js/dice.js
+++ 220618/js/dice.js
@@ -1,4 +1,5 @@
 const d4 = {
+    name: "d4",
     n: 4,
     width: 1,
     shape: "polygon(50% 6.7%, 100% 93.3%, 0% 93.3%)",
@@ -18,6 +19,7 @@ const d4 = {
 }
 
 const d6 = {
+    name: "d6",
     n: 6,
     width: 1,
     shape: "",
@@ -41,6 +43,7 @@ const d6 = {
 }
 
 const d8 = {
+    name: "d8",
     n: 8,
     width: 0.866,
     shape: "polygon(50% 0, 100% 75%, 0% 75%)",
@@ -68,6 +71,7 @@ const d8 = {
 }
 
 const d12 = {
+    name: "d12",
     n: 12,
     width: 0.95,
     shape: "polygon(19.1% 40.4%, 50% 19.1%, 80.9% 40.4%, 69.1% 75.0%, 30.9% 75.0%)",
@@ -104,6 +108,7 @@ const d12 = {
 }
 
 const d20 = {
+    name: "d20",
     n: 20,
     width: 0.866,
     shape: "polygon(50% 19.1%, 80.9% 65.5%, 19.1% 65.5%)",
@@ -151,9 +156,22 @@ const d20 = {
         "rotate3d(0,-0.305,0.4,144deg) rotateZ(-120deg) rotateX(180deg)",
         "rotate3d(0,-0.305,0.4,-144deg) rotateZ(-240deg) rotateX(180deg)",
         "rotate3d(0,-0.305,0.4,144deg) rotateZ(-240deg) rotateX(180deg)",
-    ]
+    ],
+    colorFrom: {
+        r: 255, g: 194, b: 194
+    },
+    colorTo: {
+        r: 201, g: 50, b: 50
+    }
 }
 
+function colorPicker(colorFrom, colorTo, rate) {
+    const r = colorFrom.r * rate + colorTo.r * (1 - rate);
+    const g = colorFrom.g * rate + colorTo.g * (1 - rate);
+    const b = colorFrom.b * rate + colorTo.b * (1 - rate);
+
+    return `rgb(${r}, ${g}, ${b})`
+}
 
 const diceTable = document.querySelector(".table");
 
@@ -171,6 +189,7 @@ function createDice(diceObject) {
     for (i = 0; i < diceObject.n; i++) {
         const newFace = document.createElement("div");
         newFace.classList.add("face");
+        newFace.classList.add(diceObject.name);
         newFace.style.clipPath = diceObject.shape;
         newFace.style.transformOrigin = diceObject.origin;
         newFace.style.transform = diceObject.rotation[i];
@@ -184,18 +203,14 @@ function createDice(diceObject) {
 }
 
 function createD6() { createDice(d6); }
-
 function createD4() { createDice(d4); }
-
 function createD8() { createDice(d8); }
-
 function createD12() { createDice(d12); }
-
 function createD20() { createDice(d20); }
 
 
-createD6();
 createD4();
+createD6();
 createD8();
 createD12();
 createD20();
