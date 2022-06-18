be17cf6f695ba4bef350d270bfa44ddae4d513ff
n-ryu <ababc100564@gmail.com>
2022-06-18 20:08:05 +0900

    220618
    Dice rolling program
    <Done>
    - made d4, d6, d8, d12, d20 3d structor only by css syntex
    - all dices can be generated through js code
    
    <ToDo>
      - automatically give each face different bg-color
      - interface to add and remove certain dices
      - roll at once button
      - total result indicator
      - dice lock and individual roll functions.

---
+++ 220618/js/dice.js
@@ -0,0 +1,201 @@
+const d4 = {
+    n: 4,
+    width: 1,
+    shape: "polygon(50% 6.7%, 100% 93.3%, 0% 93.3%)",
+    origin: "50% 64.4% calc(-0.204*var(--dice-size))",
+    rotation: [
+        "",
+        "rotateX(-109.472deg) rotateZ(180deg)",
+        "rotateZ(120deg) rotateX(-109.472deg) rotateZ(180deg)",
+        "rotateZ(240deg) rotateX(-109.472deg) rotateZ(180deg)",
+    ],
+    reverseRotation: [
+        "",
+        "rotateZ(180deg) rotateX(109.472deg)",
+        "rotateZ(180deg) rotateX(109.472deg) rotateZ(-120deg)",
+        "rotateZ(180deg) rotateX(109.472deg) rotateZ(-240deg)",
+    ]
+}
+
+const d6 = {
+    n: 6,
+    width: 1,
+    shape: "",
+    origin: "50% 50% calc(-0.5*var(--dice-size))",
+    rotation: [
+        "",
+        "rotateY(90deg)",
+        "rotateX(90deg)",
+        "rotateX(-90deg)",
+        "rotateY(-90deg)",
+        "rotateX(180deg)"
+    ],
+    reverseRotation: [
+        "",
+        "rotateY(-90deg)",
+        "rotateX(-90deg)",
+        "rotateX(90deg)",
+        "rotateY(90deg)",
+        "rotateX(180deg)"
+    ]
+}
+
+const d8 = {
+    n: 8,
+    width: 0.866,
+    shape: "polygon(50% 0, 100% 75%, 0% 75%)",
+    origin: "50% 50% calc(-0.345*var(--dice-size))",
+    rotation: [
+        "",
+        "rotateX(-70.528deg) rotateZ(180deg)",
+        "rotateZ(120deg) rotateX(-70.528deg) rotateZ(180deg)",
+        "rotateZ(240deg) rotateX(-70.528deg) rotateZ(180deg)",
+        "rotateX(180deg)",
+        "rotateX(180deg) rotateX(-70.528deg) rotateZ(180deg)",
+        "rotateX(180deg) rotateZ(120deg) rotateX(-70.528deg) rotateZ(180deg)",
+        "rotateX(180deg) rotateZ(240deg) rotateX(-70.528deg) rotateZ(180deg)"
+    ],
+    reverseRotation: [
+        "",
+        "rotateZ(180deg) rotateX(70.528deg)",
+        "rotateZ(180deg) rotateX(70.528deg) rotateZ(-120deg)",
+        "rotateZ(180deg) rotateX(70.528deg) rotateZ(-240deg)",
+        "rotateX(180deg)",
+        "rotateZ(180deg) rotateX(70.528deg) rotateX(180deg)",
+        "rotateZ(180deg) rotateX(70.528deg) rotateZ(-120deg) rotateX(180deg)",
+        "rotateZ(180deg) rotateX(70.528deg) rotateZ(-240deg) rotateX(180deg)"
+    ]
+}
+
+const d12 = {
+    n: 12,
+    width: 0.95,
+    shape: "polygon(19.1% 40.4%, 50% 19.1%, 80.9% 40.4%, 69.1% 75.0%, 30.9% 75.0%)",
+    origin: "50% 50% calc(-0.395*var(--dice-size))",
+    rotation: [
+        "",
+        "rotateX(-63.5deg) rotateZ(180deg)",
+        "rotateZ(72deg) rotateX(-63.5deg) rotateZ(180deg)",
+        "rotateZ(144deg) rotateX(-63.5deg) rotateZ(180deg)",
+        "rotateZ(216deg) rotateX(-63.5deg) rotateZ(180deg)",
+        "rotateZ(-72deg) rotateX(-63.5deg) rotateZ(180deg)",
+        "rotateX(180deg)",
+        "rotateX(180deg) rotateX(-63.5deg) rotateZ(180deg)",
+        "rotateX(180deg) rotateZ(72deg) rotateX(-63.5deg) rotateZ(180deg)",
+        "rotateX(180deg) rotateZ(144deg) rotateX(-63.5deg) rotateZ(180deg)",
+        "rotateX(180deg) rotateZ(216deg) rotateX(-63.5deg) rotateZ(180deg)",
+        "rotateX(180deg) rotateZ(-72deg) rotateX(-63.5deg) rotateZ(180deg)",
+    ],
+    reverseRotation: [
+        "",
+        "rotateZ(180deg) rotateX(63.5deg)",
+        "rotateZ(180deg) rotateX(63.5deg) rotateZ(-72deg)",
+        "rotateZ(180deg) rotateX(63.5deg) rotateZ(144deg)",
+        "rotateZ(180deg) rotateX(63.5deg) rotateZ(216deg)",
+        "rotateZ(180deg) rotateX(63.5deg) rotateZ(-72deg)",
+        "rotateX(180deg)",
+        "rotateZ(180deg) rotateX(63.5deg) rotateX(180deg)",
+        "rotateZ(180deg) rotateX(63.5deg) rotateZ(-72deg) rotateX(180deg)",
+        "rotateZ(180deg) rotateX(63.5deg) rotateZ(144deg) rotateX(180deg)",
+        "rotateZ(180deg) rotateX(63.5deg) rotateZ(216deg) rotateX(180deg)",
+        "rotateZ(180deg) rotateX(63.5deg) rotateZ(-72deg) rotateX(180deg)",
+
+    ]
+}
+
+const d20 = {
+    n: 20,
+    width: 0.866,
+    shape: "polygon(50% 19.1%, 80.9% 65.5%, 19.1% 65.5%)",
+    origin: "50% 50% calc(-0.4*var(--dice-size))",
+    rotation: [
+        "",
+        "rotateX(-41.8deg) rotateZ(180deg)",
+        "rotateZ(120deg) rotateX(-41.8deg) rotateZ(180deg)",
+        "rotateZ(240deg) rotateX(-41.8deg) rotateZ(180deg)",
+        "rotate3d(0,-0.305,0.4,144deg)",
+        "rotate3d(0,-0.305,0.4,-144deg)",
+        "rotateZ(120deg) rotate3d(0,-0.305,0.4,144deg)",
+        "rotateZ(120deg) rotate3d(0,-0.305,0.4,-144deg)",
+        "rotateZ(240deg) rotate3d(0,-0.305,0.4,144deg)",
+        "rotateZ(240deg) rotate3d(0,-0.305,0.4,-144deg)",
+        "rotateX(180deg)",
+        "rotateX(180deg) rotateX(-41.8deg) rotateZ(180deg)",
+        "rotateX(180deg) rotateZ(120deg) rotateX(-41.8deg) rotateZ(180deg)",
+        "rotateX(180deg) rotateZ(240deg) rotateX(-41.8deg) rotateZ(180deg)",
+        "rotateX(180deg) rotate3d(0,-0.305,0.4,144deg)",
+        "rotateX(180deg) rotate3d(0,-0.305,0.4,-144deg)",
+        "rotateX(180deg) rotateZ(120deg) rotate3d(0,-0.305,0.4,144deg)",
+        "rotateX(180deg) rotateZ(120deg) rotate3d(0,-0.305,0.4,-144deg)",
+        "rotateX(180deg) rotateZ(240deg) rotate3d(0,-0.305,0.4,144deg)",
+        "rotateX(180deg) rotateZ(240deg) rotate3d(0,-0.305,0.4,-144deg)",
+    ],
+    reverseRotation: [
+        "",
+        "rotateZ(180deg) rotateX(41.8deg)",
+        "rotateZ(180deg) rotateX(41.8deg) rotateZ(-120deg)",
+        "rotateZ(180deg) rotateX(41.8deg) rotateZ(-240deg)",
+        "rotate3d(0,-0.305,0.4,-144deg)",
+        "rotate3d(0,-0.305,0.4,144deg)",
+        "rotate3d(0,-0.305,0.4,-144deg) rotateZ(-120deg) ",
+        "rotate3d(0,-0.305,0.4,144deg) rotateZ(-120deg) ",
+        "rotate3d(0,-0.305,0.4,-144deg) rotateZ(-240deg) ",
+        "rotate3d(0,-0.305,0.4,144deg) rotateZ(-240deg) ",
+        "rotateX(180deg)",
+        "rotateZ(180deg) rotateX(41.8deg) rotateX(180deg)",
+        "rotateZ(180deg) rotateX(41.8deg) rotateZ(-120deg)rotateX(180deg)",
+        "rotateZ(180deg) rotateX(41.8deg) rotateZ(-240deg)rotateX(180deg)",
+        "rotate3d(0,-0.305,0.4,-144deg) rotateX(180deg)",
+        "rotate3d(0,-0.305,0.4,144deg) rotateX(180deg)",
+        "rotate3d(0,-0.305,0.4,-144deg) rotateZ(-120deg) rotateX(180deg)",
+        "rotate3d(0,-0.305,0.4,144deg) rotateZ(-120deg) rotateX(180deg)",
+        "rotate3d(0,-0.305,0.4,-144deg) rotateZ(-240deg) rotateX(180deg)",
+        "rotate3d(0,-0.305,0.4,144deg) rotateZ(-240deg) rotateX(180deg)",
+    ]
+}
+
+
+const diceTable = document.querySelector(".table");
+
+function createDice(diceObject) {
+    const newDiceWrapper = document.createElement("div");
+    newDiceWrapper.classList.add("dice-wrapper");
+    diceTable.appendChild(newDiceWrapper);
+
+    const newDice = document.createElement("div");
+    newDice.classList.add("dice");
+    newDice.style.width = `calc(${diceObject.width}*var(--dice-size))`;
+    newDice.style.transformOrigin = diceObject.origin;
+    newDiceWrapper.appendChild(newDice);
+
+    for (i = 0; i < diceObject.n; i++) {
+        const newFace = document.createElement("div");
+        newFace.classList.add("face");
+        newFace.style.clipPath = diceObject.shape;
+        newFace.style.transformOrigin = diceObject.origin;
+        newFace.style.transform = diceObject.rotation[i];
+        newFace.innerText = `${i + 1}`;
+        newDice.appendChild(newFace);
+    }
+
+    const result = parseInt(diceObject.n * Math.random()) + 1;
+    newDice.style.setProperty("--result", result);
+    newDice.style.transform = diceObject.reverseRotation[result - 1];
+}
+
+function createD6() { createDice(d6); }
+
+function createD4() { createDice(d4); }
+
+function createD8() { createDice(d8); }
+
+function createD12() { createDice(d12); }
+
+function createD20() { createDice(d20); }
+
+
+createD6();
+createD4();
+createD8();
+createD12();
+createD20();
